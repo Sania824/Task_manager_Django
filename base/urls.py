026e463b6cb1cff75e10c5dfd841ef426610453e
Sania824 <sania9aimen@gmail.com>
2025-08-01 17:46:20 +0500

    Added additional views and templates for the functionality

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
-from . import views
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', views.hello, name='hello')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
